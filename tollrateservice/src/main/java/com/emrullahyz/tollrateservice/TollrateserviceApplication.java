5f1ff4db73705c2225a8d7cd9c5f362e9067602d
EmrullahYz <emrullahyilmzv2@gmail.com>
2023-06-11 22:01:18 +0300

    first commit

---
+++ tollrateservice/src/main/java/com/emrullahyz/tollrateservice/TollrateserviceApplication.java
@@ -0,0 +1,13 @@
+package com.emrullahyz.tollrateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TollrateserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TollrateserviceApplication.class, args);
+	}
+
+}
---
+++ tollrateservice/src/test/java/com/emrullahyz/tollrateservice/TollrateserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emrullahyz.tollrateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TollrateserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
